fc3a2d2eceb6435edab7301535843c68f6d0bf6b
Mya <35534673+myawilliams11@users.noreply.github.com>
2018-05-01 17:55:10 -0400

    Add files via upload
    form now visible

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const multer = require('multer');
+// const mongoose = require('mongodb');
 const upload = multer({ dest: 'public/' });
 const port = 3000;
 const app = express();
 // const mongoose = require('mongoose');
 const pug= require ('pug');
 app.use(express.static('public'));
-app.listen(port);
 app.set('view engine', 'pug');
+app.set("views", "./views");
+app.use(express.urlencoded({extended: true}));
 
 // app.get/route is the actual rsvp form
 
@@ -15,11 +17,16 @@ app.set('view engine', 'pug');
 
 // app.get/guests will show list of attending and not attending
 
-var name = req.body.guest_name, name;
-var email = req.body.guest_email, email;
-var attending = req.body.guest_attendace;
-var number = req.body.total_party;
 
+app.get('/', function(req, res) {
+
+    var name = req.body.guest_name, name;
+    var email = req.body.guest_email, email;
+    var attending = req.body.guest_attendace;
+    var number = req.body.total_party;
+
+    res.render('index');
+});
 
 
 app.post('/reply', function (req, res) {
@@ -36,51 +43,19 @@ app.post('/reply', function (req, res) {
 
     res.send(`
     <h1>Thanks ${name}!</h1>
-    <p>Click here to see current <a href="/">guests</a> </p>
-    `);
-    
+    <p>Click here to see current <a href="/">/guests</a> </p>
+    `);   
 });
     
-
 
 
 app.get('/guests', function (req, res)  {
- 
     // if else for attending/ not attending?
     if (guest_attendance === yes) {
-
-        document.write(name + "yes");
-
+        document.write(name + "happily confirmed!");
     } else {
-
-        document.write(name + "no");
-
+        document.write(name + "regretfully declined.");
     }
-
 });
 
-    // app.get('/route', (req, res) => {
-    //     // const path= 
-
-        
-      
-
-    // })
-
-
-
-
-
-
-   
-    // put in pug syntax
-
-
-
- 
-
-
-
-
-// app.get('/', function (req, res) {
-// })
+app.listen(port);
