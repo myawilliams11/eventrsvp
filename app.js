e1ee501d869d27ea26cf85b168bf5489e49eda53
Mya <35534673+myawilliams11@users.noreply.github.com>
2018-05-02 12:01:54 -0400

    Add files via upload
    final commit

--- app.js
+++ app.js
@@ -1,61 +1,71 @@
+const mongoose = require('mongoose');
+
 const express = require('express');
-const multer = require('multer');
-// const mongoose = require('mongodb');
-const upload = multer({ dest: 'public/' });
+
 const port = 3000;
 const app = express();
 // const mongoose = require('mongoose');
 const pug= require ('pug');
 app.use(express.static('public'));
 app.set('view engine', 'pug');
-app.set("views", "./views");
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({extended: false}));
 
-// app.get/route is the actual rsvp form
+const formSchema = mongoose.Schema({
+    name: String,
+    email: String,
+    attending: Boolean,
+    guests: Number
+});
 
-// app.post/reply is "Thank you for your response" with a link to guest list
-
-// app.get/guests will show list of attending and not attending
-
+const Response = mongoose.model('Response', formSchema);
 
 app.get('/', function(req, res) {
-
-    var name = req.body.guest_name, name;
-    var email = req.body.guest_email, email;
-    var attending = req.body.guest_attendace;
-    var number = req.body.total_party;
 
     res.render('index');
 });
 
 
 app.post('/reply', function (req, res) {
-    let instance = new req.response({ 
-        // do something to response on 18
-        name: req.name,
-        email: req.email,
-        attending: req.attending,
-        guests: req.guests,   
-    });
-    instance.save(function (err, instance) {
-        if (err) return console.error(err);
-    })
+    // console.log(req.body);
+    const instance = new Response(req.body);
 
-    res.send(`
-    <h1>Thanks ${name}!</h1>
-    <p>Click here to see current <a href="/">/guests</a> </p>
-    `);   
+
+    // Callbacks 
+
+    // instance.save(function (err, instance) {
+    //     if (err) res.send(err);
+    //     res.send(`
+    //     <h1>Thank your for your response!</h1>
+    //     <p>Click here to see current <a href="/guests">/guests</a> </p>
+    //   `);
+    // })
+
+    // Promises
+
+    instance.save()
+        .then(instance => res.send(`
+            <h1>Thank your for your response!</h1>
+            <p>Click here to see current <a href="/guests">/guests</a> </p>
+        `)).catch(err => res.send(err))
+
+       
 });
     
 
 
 app.get('/guests', function (req, res)  {
     // if else for attending/ not attending?
-    if (guest_attendance === yes) {
-        document.write(name + "happily confirmed!");
-    } else {
-        document.write(name + "regretfully declined.");
-    }
+    Response.find( function (err, people) {
+    
+    res.render("guests", {people});
+   
+    // document.write(people[i].name + "happily confirmed!"); 
+    
+   
+});
 });
 
-app.listen(port);
+
+app.listen(port, () => {
+    mongoose.connect('mongodb://localhost/rsvp');
+});
